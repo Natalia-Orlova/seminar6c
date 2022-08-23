cac23ef2f6c6be784b8eb6e6a695d0850077ed58
NataliaOrlova <natakarelskaya@yandex.ru>
2022-08-23 11:34:34 +0400

    разворот с циклом for

--- task0/Program.cs
+++ task0/Program.cs
@@ -18,34 +18,45 @@ int[] CreateArrayRndInt(int size, int min, int max)
     return array;
 }
 
-void PrintArray (int[] array)
+void PrintArray(int[] array)
 {
     Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     {
-       if(i < array.Length - 1) Console.Write($"{array[i]}, ");
-       else Console.Write(array[i]);
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write(array[i]);
     }
     Console.WriteLine("]");
 }
 
-void ReverseArray (int[] array)
+// void ReverseArray (int[] array)
+// {
+//     int size = array.Length;
+//     int index1 = 0;
+//     int index2 = size - 1;
+
+//     while (index1 < index2)
+//     {
+//         int obj = array[index1];
+//         array[index1] = array[index2];
+//         array[index2] = obj;
+//         index1++;
+//         index2--;
+//     }
+// }
+
+void ReverseArray1(int[] array)
 {
     int size = array.Length;
-    int index1 = 0;
-    int index2 = size - 1;
-
-    while (index1 < index2)
+    for (int i = 0; i < size/2; i++)
     {
-        int obj = array[index1];
-        array[index1] = array[index2];
-        array[index2] = obj;
-        index1++;
-        index2--;
+        int temp = array[i];
+        array[i] = array[size - 1 - i];
+        array[size - 1 - i] = temp;
     }
 }
 
 int[] arr = CreateArrayRndInt(5, 1, 9);
 PrintArray(arr);
-ReverseArray(arr);
+ReverseArray1(arr);
 PrintArray(arr);
